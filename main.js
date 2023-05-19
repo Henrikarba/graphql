32b729a42b7427cda3356012278124325d16c76c
Henri <henrikarba@gmail.com>
2023-05-20 02:43:47 +0300

    auto commit

--- main.js
+++ main.js
@@ -24,38 +24,35 @@ loginForm.addEventListener('submit', async (e) => {
         });
 
         if (response.ok) {
-            // Successful login
-            const { token } = await response.json();
-            console.log(token);
-            // Store the JWT securely (e.g., in local storage or a secure cookie) for future API requests
+            const token = await response.json();
+            // Store the JWT in local storage for future API requests
             localStorage.setItem('jwt', token);
-            // Redirect or perform any other actions after successful login
             const app = document.getElementById('app');
             app.innerHTML = `
-                <section class="user-identification">
-                    <p id="user-id"></p>
-                    <p id="user-login"></p>
-                </section>
-
-                <section class="xp-amount">
-                    <!-- Display XP amount information -->
-                </section>
-
-                <section class="grades">
-                    <!-- Display grades information -->
-                </section>
-
-                <section class="audits">
-                    <!-- Display audits information -->
-                </section>
-
-                <section class="skills">
-                    <!-- Display skills information -->
-                </section>
-
-                <section class="statistics">
-                    <!-- Add the necessary elements to display statistic graphs -->
-                </section>
+                <div class="profile-container">
+                    <h1>My Profile</h1>
+                    <div class="basic-info">
+                        <h2>Basic Information</h2>
+                        <p><strong>Name:</strong> <span id="name"></span></p>
+                        <p><strong>Email:</strong> <span id="email"></span></p>
+                        <p><strong>From:</strong> <span id="from"></span></p>
+                        <p><strong>Phone:</strong> <span id="phone"></span></p>
+                    </div>
+                    <div class="xp-progress">
+                        <h2>Progress Over Time</h2>
+                    </div>
+                    <div class="skill">
+                        <h2>Skill</h2>
+                    </div>
+                    <div class="tasks">
+                        <h2>Tasks</h2>
+                    </div>
+                    <div class="audits">
+                        <h2>Audits Ratio</h2>
+                        <p>Audit Ratio: <span id="audit-ratio"></span></p>
+                    </div>
+                    <!-- Add more sections and components as needed -->
+                </div>
             `;
             // Example GraphQL query
             const query = `
@@ -73,19 +70,9 @@ loginForm.addEventListener('submit', async (e) => {
                             userId
                             type
                             amount
-                            attrs
                             createdAt
                             path
                         }
-                    }
-                    transaction (order_by: { createdAt: asc }) {
-                        id
-                        type
-                        amount
-                        userId
-                        attrs
-                        createdAt
-                        path
                     }
                 }`;
             // Make the GraphQL API request and process the response
@@ -107,7 +94,7 @@ function displayError(message) {
     errorContainer.style.display = 'block';
 }
 
-// Function to make the GraphQL API request
+// Make the GraphQL API request
 async function makeGraphQLRequest(query) {
     const endpoint = 'https://01.kood.tech/api/graphql-engine/v1/graphql';
     const jwt = localStorage.getItem('jwt');
@@ -116,14 +103,40 @@ async function makeGraphQLRequest(query) {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': jwt
+            'Authorization': `Bearer ${jwt}`
         },
         body: JSON.stringify({ query }),
     })
         .then(response => response.json())
         .then(result => {
-            console.log(result)
-            return result.data;
+            // Populate the profile page with the retrieved data
+            console.log(result.data)
+            const { email, firstName, lastName, tel, addressCity, addressCountry } = result.data.user[0].attrs;
+            const { totalDown, totalUp } = result.data.user[0];
+            const transactions = result.data.user[0].transactions;
+            let totalXp = 0;
+            for (let i = 0; i < transactions.length; i++) {
+                const { type, amount } = transactions[i];
+                if (type === "xp") {
+                    totalXp = totalXp + amount;
+                }
+            }
+            const auditRatio = totalUp / totalDown;
+            document.getElementById('name').textContent = firstName + " " + lastName;
+            document.getElementById('email').textContent = email;
+            document.getElementById('from').textContent = addressCity + "," + addressCountry;
+            document.getElementById('phone').textContent = tel;
+            document.getElementById('xp').textContent = totalXp;
+            document.getElementById('audit-ratio').textContent = `${auditRatio.toFixed(1)}`;
+
+            /*// Populate grades
+            const gradesList = document.getElementById('grades-list');
+            grades.forEach((grade) => {
+                const listItem = document.createElement('li');
+                listItem.textContent = `${grade.subject}: ${grade.grade}`;
+                gradesList.appendChild(listItem);
+            });*/
+            window.addEventListener('DOMContentLoaded', fetchAndPopulateProfileData);
         })
         .catch(error => {
             console.error("Error:", error)
